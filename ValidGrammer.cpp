dff33d2d535e5679dc4807d448ce85b06038f97a
Yasiralam <yasiralam981@gmail.com>
2024-04-25 12:02:50 +0530

    Fix:ValidGrammer.cpp file adn added redunded tokens and optimise the tokens and check weather grammer is valid or not.

--- ValidGrammer.cpp
+++ ValidGrammer.cpp
@@ -11,7 +11,8 @@ using namespace std;
 
 */
 
- vector<int> GetTokens(string grammer,vector<int> v){
+ vector<int> GetTokens(string &grammer){
+    vector<int> v;
     int i=0;
     while(i<grammer.length()){
         if(grammer[i]=='(' || grammer[i]=='{'){
@@ -36,53 +37,65 @@ using namespace std;
                 }
                 v.push_back(6);
         }
-    
+        i++;
+
     }
+     return v;
 }
+
 vector<int> optimergrammer(vector<int> &tokenvector) {
-    int i = 0;
-    while (i <= tokenvector.size() - 2) {
-        if (tokenvector[i] == 6 && tokenvector[i + 1] == 2 && tokenvector[i + 2] == 6) {
-            tokenvector[i] = 6;
-            tokenvector.erase(tokenvector.begin() + i + 1, tokenvector.begin() + i + 3);
-        }
-        else if (tokenvector[i] == 6 && tokenvector[i + 1] == 3 && tokenvector[i + 2] == 6) {
-            tokenvector[i] = 6;
-            tokenvector.erase(tokenvector.begin() + i + 1, tokenvector.begin() + i + 3);
-        }
-        else if (tokenvector[i] == 4 && tokenvector[i + 1] == 6 && tokenvector[i + 2] == 5) {
-            tokenvector[i] = 6;
-            tokenvector.erase(tokenvector.begin() + i + 1, tokenvector.begin() + i + 3);
-        }
-        else {
-            i++;
+    bool optimized = true;
+    while (optimized) {
+        optimized = false; // Reset kro flag ko at beginning of each iteration
+        for (int i = 0; i <= tokenvector.size() - 2; ++i) {
+            if (tokenvector[i] == 6 && tokenvector[i + 1] == 6) {
+                tokenvector.erase(tokenvector.begin() + i + 1); // Remove the consecutive 6
+                optimized = true; // Set kro flag ko true 
+                break; 
+            }
+            else if (tokenvector[i] == 6 && tokenvector[i + 1] == 2 && tokenvector[i + 2] == 6) {
+                tokenvector[i] = 6;
+                tokenvector.erase(tokenvector.begin() + i + 1, tokenvector.begin() + i + 3);
+                optimized = true;
+                break;
+            }
+            else if (tokenvector[i] == 6 && tokenvector[i + 1] == 3 && tokenvector[i + 2] == 6) {
+                tokenvector[i] = 6;
+                tokenvector.erase(tokenvector.begin() + i + 1, tokenvector.begin() + i + 3);
+                optimized = true;
+                break;
+            }
+            else if (tokenvector[i] == 4 && tokenvector[i + 1] == 6 && tokenvector[i + 2] == 5) {
+                tokenvector[i] = 6;
+                tokenvector.erase(tokenvector.begin() + i + 1, tokenvector.begin() + i + 3);
+                optimized = true;
+                break;
+            }
         }
     }
     return tokenvector;
 }
+
 int main(){
 
-vector<int> v(20);
-vector<int> tokenvector(20);
-vector<int> result(20);
-string grammer;
-cout<<"Enter your grammer:"<<endl;
-getline(cin,grammer);
-tokenvector = GetTokens(grammer,v);
-for (int i = 0; i < tokenvector.size(); i++)
-{
-   cout<<tokenvector[i];
-}
-
-result = optimergrammer(tokenvector);
-for (int i = 0; i < result.size(); i++)
-{
-    cout<<result[i]<<" ";
-}
-
-
-
-
-
+        
+        vector<int> tokenvector;
+        vector<int> result(30);
+        string grammer;
+        cout<<"Enter your grammer:"<<endl;
+        getline(cin,grammer);
+        tokenvector = GetTokens(grammer);
+        cout<<"Token Generated:"<<endl;
+        for (int i = 0; i < tokenvector.size(); i++)
+        {
+           cout<<tokenvector[i];
+        }
+        cout<<endl;
+        cout<<"Result:"<<endl;
+        result = optimergrammer(tokenvector);
+        for (int i = 0; i < result.size(); i++)
+        {
+            cout<<result[i]<<" ";
+        }
 return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+lt = optimergrammer(tokenvector);
+        // for (int i = 0; i < result.size(); i++)
+        // {
+        //     cout<<result[i]<<" ";
+        // }
